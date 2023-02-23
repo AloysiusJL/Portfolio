0965780c26d4ba0d633c3309ba1411b102edcfa6
Aloysius Jeremy Laksono <100062287+MrBego@users.noreply.github.com>
2023-02-24 03:32:43 +0700

    Add files via upload
    1st Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var welcome = new Typed(".welcome", {
+    strings: ["Welcome!"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
+
+
+// gsap.registerPlugin(ScrollTrigger)
+
+// gsap.to(".name", {
+//     x: 100,
+//     duration: 3,
+//     scrollTrigger: {
+//         trigger: ".name",
+//         start: "top 80%",
+//         end: "top 30%",
+//         scrub: 4,
+//         toggleActions: "restart none none none",
+//         markers: true
+//     }
+// })
+
